79f15b7a2e7a531828002d238fa49c7ce6cd3ff4
Sergei <reyangood@gmail.com>
2024-02-26 03:40:57 +0300

    add forms

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,8 +1,23 @@
 from django.urls import path
 
-from hexlet_django_blog.article.views import IndexView, ArticleView
+from hexlet_django_blog.article.views import (
+    IndexView,
+    ArticleView,
+    ArticleFormEditView,
+    ArticleFormDeleteView,
+)
+from hexlet_django_blog.article.views import ArticleFormCreateView
 
 urlpatterns = [
-    path('', IndexView.as_view()),
+    path('', IndexView.as_view(), name='article_index'),
+    path(
+        '<int:id>/edit/', ArticleFormEditView.as_view(), name='articles_update'
+    ),
+    path(
+        '<int:id>/delete/',
+        ArticleFormDeleteView.as_view(),
+        name='articles_delete',
+    ),
     path('<int:id>/', ArticleView.as_view(), name='article'),
+    path('create/', ArticleFormCreateView.as_view(), name='articles_create'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,7 +1,9 @@
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
-from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from hexlet_django_blog.article.models import Article
+from .forms import ArticleForm
 
 
 class IndexView(View):
@@ -12,9 +14,7 @@ class IndexView(View):
         return render(
             request,
             'articles/index.html',
-            context={
-                'articles': articles,
-            },
+            context={'articles': articles},
         )
 
 
@@ -28,3 +28,52 @@ class ArticleView(View):
                 'article': article,
             },
         )
+
+
+class ArticleFormCreateView(View):
+    def get(self, request):
+        form = ArticleForm()
+        return render(request, 'articles/create.html', {'form': form})
+
+    def post(self, request):
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Статья успешно добавлена')
+            
+            return redirect('article_index')
+
+        return render(request, 'articles/create.html', {'form': form})
+
+
+class ArticleFormEditView(View):
+    def get(self, request, id):
+        article = Article.objects.get(id=id)
+        form = ArticleForm(instance=article)
+        return render(
+            request,
+            'articles/update.html',
+            {'form': form, 'article_id': id},
+        )
+
+    def post(self, request, id):
+        article = Article.objects.get(id=id)
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('article_index')
+
+        return render(
+            request,
+            'articles/update.html',
+            {'form': form, 'article_id': id},
+        )
+
+
+class ArticleFormDeleteView(View):
+    def post(self, request, id):
+        article = Article.objects.get(id=id)
+        if article:
+            article.delete()
+            messages.success(request, 'Статья успешно удалена')
+        return redirect('article_index')
